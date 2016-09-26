d6698f8d77dc9e50943b299d3a22dbbe4bf88de8
taroufelinxg1305 <wichoprada13_05@hotmail.com>
2016-09-26 12:04:54 -0500

    cargado el cliente y servidor TCP

---
+++ EmbebidoBRT/src/ServidorTCP/NmeatoJson.java
@@ -0,0 +1,92 @@
+package ServidorTCP;
+
+import java.util.StringTokenizer;
+
+public class NmeatoJson {
+
+	private static double[][] lastCoor = new double[10][2]; // matriz que
+															// contiene las diez
+															// ultimas lecturas
+															// de latitud y
+															// longitud
+	static int tamLastcoor = 0; // indice para recorrer la matriz
+	/*
+	 * metodo que recibe un string(GGA nmea) y de el se estraen latitud y
+	 * longitud absolutas
+	 */
+
+	public static void SepararToken(String s) {
+
+		// entrada=
+		// "$GPGGA,181908.00,3404.7041778,N,07044.3966270,W,4,13,1.00,495.144,M,29.200,M,0.10,0000*40";
+		String entrada = s;
+		StringTokenizer st = new StringTokenizer(entrada, ",");
+		String[] tok = new String[st.countTokens()]; // vector par almacenar
+														// cada datos separado
+														// por coma de la
+														// entrada
+		int cont = 0;
+
+		while (st.hasMoreTokens()) {
+
+			tok[cont] = st.nextToken();
+			cont++;
+		}
+		String lat = "", lon = ""; // en base a tok[2] y tok[3] lleno lat, en
+									// base a tok[4] y tok[5] lleno lon
+		if (tok[3].equals("N"))
+			lat = tok[2];
+		else if (tok[3].equals("S"))
+			lat = "-" + tok[2]; // si la latitud pertenece al hemisferio sur, la
+								// coordenada absoluta de latitud es negatica
+		if (tok[5].equals("E"))
+			lon = tok[4];
+		else if (tok[5].equals("W"))
+			lon = "-" + tok[4]; // si la longitud pertene al hemisferio oeste,
+								// la coordenada absoluta de longitud es
+								// negativa
+
+		System.out.println("latitud:  " + lat);
+
+		System.out.println("longitud: " + lon);
+		String linea = "" + lat + "," + lon;
+		Operaciones.guardar(linea); // almacena un historico de los pares de
+									// coordenadas absolutas(latitud,longitud)
+		almacenTemp(lat, lon);
+
+	}
+
+	/*
+	 * almacena las diez ultimos pares coordenadas(latitud, longitud)
+	 */
+	private static void almacenTemp(String s1, String s2) {
+		if (tamLastcoor < 10) {
+			try {
+				lastCoor[tamLastcoor][0] = Double.parseDouble(s1);
+				lastCoor[tamLastcoor][1] = Double.parseDouble(s2);
+				tamLastcoor++;
+			} catch (NumberFormatException n) {
+				System.out.println("fallo la conversion de coordenada");
+			}
+
+		} else {
+			for (int i = 0; i < tamLastcoor - 1; i++) {
+				lastCoor[i][0] = lastCoor[i + 1][0];
+				lastCoor[i][1] = lastCoor[i + 1][1];
+			}
+
+			try {
+				lastCoor[tamLastcoor - 1][0] = Double.parseDouble(s1);
+				lastCoor[tamLastcoor - 1][1] = Double.parseDouble(s2);
+			} catch (NumberFormatException n) {
+				System.out.println("fallo la conversion de coordenada");
+			}
+		}
+
+	}
+
+	public static double[][] getLastCoor() {
+		return lastCoor;
+	}
+
+}
---
+++ EmbebidoBRT/src/ServidorTCP/SampleServer.java
@@ -0,0 +1,63 @@
+package ServidorTCP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class SampleServer {
+
+	static double latiprom =0; //son los valores promedio que se enviaran en algun momento a la plataforma rutasBuses
+	static double longprom=0;
+	static int moduloPromedio=0;
+    public static void main(String[] args) throws IOException {
+    	// Define que el socket escuchara en el puerto 9091
+        ServerSocket listener = new ServerSocket(9091);
+        double[] promedios= new double[2];
+        try {
+            while (true) {
+            	// Abre el socket y acepta las conexiones
+                Socket socket = listener.accept(); //verifica si la conexion fu exitosa
+                System.out.print("Server has connected!\n");
+
+                try {
+                	
+                	BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+                	
+                    while (true) {
+                    	// Lee la linea de texto que llega del cliente
+                        String input = in.readLine();                        
+                        if (input == null || input.equals(".")) {
+                            break;
+                        }
+                        // Hace el tratamiento al texto recibido, en este caso imprimir en la consola
+                        
+                        //System.out.println(input);
+                     NmeatoJson.SepararToken(input);
+                     if(moduloPromedio==9) //cada 10 lineas leidas y enviadas por el cliente hace la operacion promedio
+                     {
+                    	 moduloPromedio=0;
+                    	 //trae el arreglo que almacena las ultimas 10 coordenadas
+                    	 promedios= Operaciones.promedioCoor(NmeatoJson.getLastCoor());
+                    	 latiprom=promedios[0];
+                    	 longprom=promedios[1];
+                    	 System.out.println("promedios: "+ latiprom+","+longprom);
+                     }
+                     else moduloPromedio++;
+                    }
+                } finally {
+                    socket.close();
+                }
+            }
+        }
+        finally {
+            listener.close();
+        }
+        
+    }
+
+
+}
